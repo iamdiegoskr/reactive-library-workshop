4f4041718c67e299fc86a870716ace26e7edbbc3
Diego Martinez <juandiego337@hotmail.com>
2021-10-17 15:13:50 -0500

    Fix details in use case list get resources

--- src/main/java/com/sofka/bibliotecaskr/usecases/ListAllResourcesUseCase.java
+++ src/main/java/com/sofka/bibliotecaskr/usecases/ListAllResourcesUseCase.java
@@ -3,10 +3,14 @@ package com.sofka.bibliotecaskr.usecases;
 import com.sofka.bibliotecaskr.dtos.ResourceDTO;
 import com.sofka.bibliotecaskr.mappers.MappersUtils;
 import com.sofka.bibliotecaskr.repositories.ResourceRepository;
+import org.springframework.stereotype.Service;
+import org.springframework.validation.annotation.Validated;
 import reactor.core.publisher.Flux;
 
 import java.util.function.Supplier;
 
+@Service
+@Validated
 public class ListAllResourcesUseCase implements Supplier<Flux<ResourceDTO>> {
 
     private final ResourceRepository repository;
